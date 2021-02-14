cdc7e52cd45b4553dedb3d723093dc89c51dfd6d
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-02-14 23:43:02 +0700

    update tombol bawah

--- src/components/BottomNavComponent.js
+++ src/components/BottomNavComponent.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import Skeleton from 'react-loading-skeleton'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, Route } from 'react-router-dom'
 import homeIcon from '../assets/img/icon/home.svg'
 import menuIcon from '../assets/img/icon/menu.svg'
 import noteIcon from '../assets/img/icon/note.svg'
@@ -37,62 +37,70 @@ export class BottomNavComponent extends Component {
             <nav className="navbar navbar-expand-lg desktop-navigation other-navbar p-0 d-block fixed-bottom">
                <div className="container">
                   <ul className="menu-mobile-bottom">
-                     <li className="img-nav">
-                        <NavLink to='/'>
-                           <div>
-                              {!this.state.homeIconLoaded &&
-                                 <Skeleton />
+                     <Route path='/' exact children={({ match }) => (
+                        <li className={match ? 'img-nav active' : 'img-nav'}>
+                           <Link to='/'>
+                              <div>
+                                 {!this.state.homeIconLoaded &&
+                                    <Skeleton />
+                                 }
+                                 <img src={homeIcon} onLoad={this.handleLoadHomeIcon.bind(this)} alt='Staner.id' height="24" />
+                              </div>
+                              {!this.state.homeIconLoaded
+                                 ? <Skeleton />
+                                 : <small className="text-dark fs-10 mt-0">Beranda</small>
                               }
-                              <img src={homeIcon} onLoad={this.handleLoadHomeIcon.bind(this)} alt='Staner.id' height="24" />
-                           </div>
-                           {!this.state.homeIconLoaded
-                              ? <Skeleton />
-                              : <small className="text-dark fs-10 mt-0">Beranda</small>
-                           }
-                        </NavLink>
-                     </li>
-                     <li className="img-nav">
-                        <NavLink to="/info">
-                           <div>
-                              {!this.state.noteIconLoaded &&
-                                 <Skeleton />
+                           </Link>
+                        </li>
+                     )} />
+                     <Route path='/info' children={({ match }) => (
+                        <li className={match ? 'img-nav active' : 'img-nav'}>
+                           <Link to='/info'>
+                              <div>
+                                 {!this.state.noteIconLoaded &&
+                                    <Skeleton />
+                                 }
+                                 <img src={noteIcon} onLoad={this.handleLoadNoteIcon.bind(this)} alt='Staner.id' height="24" />
+                              </div>
+                              {!this.state.noteIconLoaded
+                                 ? <Skeleton />
+                                 : <small className="text-dark fs-10 mt-0">Info</small>
                               }
-                              <img src={noteIcon} onLoad={this.handleLoadNoteIcon.bind(this)} alt='Staner.id' height="24" />
-                           </div>
-                           {!this.state.noteIconLoaded
-                              ? <Skeleton />
-                              : <small className="text-dark fs-10 mt-0">Info</small>
-                           }
-                        </NavLink>
-                     </li>
-                     <li className="img-nav">
-                        <NavLink to="/event">
-                           <div>
-                              {!this.state.menuIconLoaded &&
-                                 <Skeleton />
+                           </Link>
+                        </li>
+                     )} />
+                     <Route path='/event' children={({ match }) => (
+                        <li className={match ? 'img-nav active' : 'img-nav'}>
+                           <Link to='/event'>
+                              <div>
+                                 {!this.state.menuIconLoaded &&
+                                    <Skeleton />
+                                 }
+                                 <img src={menuIcon} onLoad={this.handleLoadMenuIcon.bind(this)} alt='Staner.id' height="24" />
+                              </div>
+                              {!this.state.menuIconLoaded
+                                 ? <Skeleton />
+                                 : <small className="text-dark fs-10 mt-0">Acara</small>
                               }
-                              <img src={menuIcon} onLoad={this.handleLoadMenuIcon.bind(this)} alt='Staner.id' height="24" />
-                           </div>
-                           {!this.state.menuIconLoaded
-                              ? <Skeleton />
-                              : <small className="text-dark fs-10 mt-0">Acara</small>
-                           }
-                        </NavLink>
-                     </li>
-                     <li className="img-nav">
-                        <NavLink to='/my-account'>
-                           <div>
-                              {!this.state.userIconLoaded &&
-                                 <Skeleton />
+                           </Link>
+                        </li>
+                     )} />
+                     <Route path='/my-account' children={({ match }) => (
+                        <li className={match ? 'img-nav active' : 'img-nav'}>
+                           <Link to='/my-account'>
+                              <div>
+                                 {!this.state.userIconLoaded &&
+                                    <Skeleton />
+                                 }
+                                 <img src={userIcon} onLoad={this.handleLoadUserIcon.bind(this)} alt='Staner.id' height="24" />
+                              </div>
+                              {!this.state.userIconLoaded
+                                 ? <Skeleton />
+                                 : <small className="text-dark fs-10 mt-0">Akun</small>
                               }
-                              <img src={userIcon} onLoad={this.handleLoadUserIcon.bind(this)} alt='Staner.id' height="24" />
-                           </div>
-                           {!this.state.userIconLoaded
-                              ? <Skeleton />
-                              : <small className="text-dark fs-10 mt-0">Akun</small>
-                           }
-                        </NavLink>
-                     </li>
+                           </Link>
+                        </li>
+                     )} />
                   </ul>
                </div>
             </nav>

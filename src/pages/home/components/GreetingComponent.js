f892909789bfe8e6c3b5477bf0e8172e095c82c7
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-02-16 23:44:42 +0700

    Update GreetingComponent.js

--- src/pages/home/components/GreetingComponent.js
+++ src/pages/home/components/GreetingComponent.js
@@ -1,35 +1,54 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import Skeleton from 'react-loading-skeleton'
 
 import sendIcon from '../../../assets/img/icon/send.svg'
 
-export default class GreetingComponent extends Component {
-   state = {
-      imageLoaded: false
+function GreetingComponent() {
+   const [imageLoaded, setImageLoaded] = useState(false)
+   const [greeting, setGreeting] = useState('')
+
+   function handleImageLoaded() {
+      setImageLoaded(true)
    }
 
-   handleImageLoaded = () => {
-      this.setState({
-         imageLoaded: true
-      })
+   function getHour() {
+      const date = new Date()
+      const hour = date.hour
+      let greeting = ''
+      
+      if (hour > 0 && hour < 12) {
+        greeting = 'Selamat pagi.'
+      } else if (hour >= 12 && hour < 15) {
+        greeting = 'Selamat siang.'
+      } else if (hour >= 15 && hour < 18) {
+        greeting = 'Selamat sore.'
+      } else {
+        greeting = 'Selamat malam.'
+      }
+
+      setGreeting(greeting)
    }
 
-   render() {
-      return (
-         <div className="card-body px-2 pt-0">
-            <div className="card-author no-border align-items-center p-0">
-               { !this.state.imageLoaded &&
-                  <Skeleton/>
-               }
-               <img src={sendIcon} style={{ padding: '3px', borderRadius: 0, border: '0 !important' }} className="img-fluid img-profile mr-3" alt="Halo, Staner!" onLoad={this.handleImageLoaded.bind(this)} />
-               <div className="info-profile">
-                  <p className="title pb-0 pl-0">Halo, Staner!</p>
-                  <div className="d-flex align-items-center mb-0">
-                     <p className="text-default mb-0 mr-2">Selamat siang.</p>
-                  </div>
+   useEffect(() => {
+      getHour()
+   }, [])
+
+   return (
+      <div className="card-body px-2 pt-0">
+         <div className="card-author no-border align-items-center p-0">
+            { !imageLoaded &&
+               <Skeleton/>
+            }
+            <img src={sendIcon} style={{ padding: '3px', borderRadius: 0, border: '0 !important' }} className="img-fluid img-profile mr-3" alt="Halo, Staner!" onLoad={handleImageLoaded.bind(this)} />
+            <div className="info-profile">
+               <p className="title pb-0 pl-0">Halo, Staner!</p>
+               <div className="d-flex align-items-center mb-0">
+                  <p className="text-default mb-0 mr-2">{greeting}</p>
                </div>
             </div>
          </div>
-      )
-   }
+      </div>
+   )
 }
+
+export default GreetingComponent

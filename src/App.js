bc72e2434468843e78b1acba6f0fed61f40a0bb5
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-02-16 22:19:09 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Fragment, lazy, Suspense } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React, { Fragment, lazy, Suspense, useEffect } from 'react'
+import { BrowserRouter, Route, useLocation } from 'react-router-dom'
 import BottomNavComponent from './components/BottomNavComponent'
 import TopNavComponent from './components/TopNavComponent'
 
@@ -7,10 +7,21 @@ const Home = lazy(() => import('./pages/home/Home'))
 const Info = lazy(() => import('./pages/info/Info'))
 const Event = lazy(() => import('./pages/event/Event'))
 
+export function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Fragment>
       <BrowserRouter>
+        <ScrollToTop />
         <TopNavComponent/>
           <Route exact path='/' component={() => <Suspense fallback={<div>Loading...</div>}><Home /></Suspense>} />
           <Route path='/info' component={() => <Suspense fallback={<div>Loading...</div>}><Info /></Suspense>} />
